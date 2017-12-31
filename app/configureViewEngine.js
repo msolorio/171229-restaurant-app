cf3b75093bf003d10b669a78610aed313a4c4927
msolorio <msolorio789@gmail.com>
2017-12-30 18:20:51 -0800

    refactor template organization

--- app/configureViewEngine.js
+++ app/configureViewEngine.js
@@ -1,4 +1,9 @@
+function getRestaurantName() {
+  return 'Lupita\'s';
+}
+
 function configureViewEngine(app, hbs) {
+  hbs.registerHelper('getRestaurantName', getRestaurantName);
   hbs.registerPartials(`${__dirname}/../views/partials`);
   app.set('view engine', 'hbs');
 }
