0c45f984f0bcdcb87124c6552b7e4f6e57f88e74
msolorio <msolorio789@gmail.com>
2018-01-06 11:36:28 -0800

    test server.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,24 @@ const indexRouter = require('../routes');
 
 const app = express();
 
-configureViewEngine(app, hbs);
-serveStatic(app);
+function init() {
+  configureViewEngine(app, hbs);
 
-app.use(logger);
+  serveStatic(app);
 
-app.use(indexRouter);
+  app.use(logger);
 
-app.listen(3001, () => console.log('your app is listening on port 3001'));
+  app.use(indexRouter);
+};
+
+init();
+
+// if file is run directly start server
+// wont start server for unit tests
+if (require.main === module) {
+  app.listen(3001, () => {
+    console.log('your app is listening on port 3001');
+  });
+}
+
+module.exports = { app, init };
