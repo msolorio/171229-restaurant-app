24fec79a7b985550dfc249a35929663c1abf018e
msolorio <msolorio789@gmail.com>
2018-01-09 22:47:36 -0800

    extend tests for configureViewEngine index

--- app/configureViewEngine/index.spec.js
+++ app/configureViewEngine/index.spec.js
@@ -14,21 +14,31 @@ const app = { set: jest.fn() };
 const hbs = 'handlebarsInstance';
 
 describe('configureViewEngine', () => {
+
+  beforeEach(() => {
+    registerPartials.mockClear();
+    registerHelpers.mockClear();
+    app.set.mockClear();
+  });
+
   it('should call registerHelpers with proper arguments', () => {
     configureViewEngine(app, hbs);
 
+    expect(registerHelpers).toHaveBeenCalledTimes(1);
     expect(registerHelpers).toHaveBeenCalledWith(hbs);
   });
 
   it('should call registerPartials with proper arguments', () => {
     configureViewEngine(app, hbs);
 
+    expect(registerPartials).toHaveBeenCalledTimes(1);
     expect(registerPartials).toHaveBeenCalledWith(hbs);
   });
 
   it('should call app.set with proper arguments', () => {
     configureViewEngine(app, hbs);
 
+    expect(app.set).toHaveBeenCalledTimes(1);
     expect(app.set).toHaveBeenCalledWith('view engine', 'hbs');
   });
 });
